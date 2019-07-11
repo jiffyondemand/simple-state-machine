4b24a70a65de828687bfcebb6fff0a537790e74c
Michael Choi <michaeljkchoi@gmail.com>
2019-07-11 16:50:19 -0400

    Small changes to ssm

--- lib/ssm.rb
+++ lib/ssm.rb
@@ -1,6 +1,5 @@
 require "ssm/errors"
 require "ssm/event"
-require "ssm/validation"
 require "ssm/version"
 
 require "ssm/ssm"
--- lib/ssm/ssm.rb
+++ lib/ssm/ssm.rb
@@ -11,6 +11,15 @@ module SSM
       end
     end
 
+    def state_validation(*states, &block)
+      states.each do |state|
+        is_state = (state.to_s + "?").to_sym
+        with_options if: is_state do |resource|
+          yield resource
+        end
+      end
+    end
+
     private
       def define_state_boolean(_state)
         method_name = "#{ _state }?"
